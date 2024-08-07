c169bb310bafa051adbb893d196c61221b8c9de7
Godwin Bruce <godwin.bruce@gmail.com>
2024-08-07 06:00:10 +0000

    updates

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,14 @@
+xport default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(obj) {
+      let count = 0;
+      for (const key in obj) {
+        if (Object.hasOwnProperty.call(obj, key)) {
+          count += 1;
+        }
+      }
+      return count;
+    },
+  };
+}
